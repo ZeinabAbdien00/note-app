1ace93a6aee3945e1097c8b5733652e8706fbfc8
Zeinab Abdien <suzanabdien15@gmail.com>
2023-07-30 16:41:09 +0300

    activity code

---
+++ app/src/main/java/com/iti/android_7/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.iti.android_7.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.iti.android_7.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
